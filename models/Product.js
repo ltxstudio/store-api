83097de027aac6bfa0135729611e7c5e7a79fc1c
ltxstudio <ltxstudio.info@gmail.com>
2024-11-14 18:34:36 +0600

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -6,6 +6,9 @@ const productSchema = new mongoose.Schema({
   price: { type: Number, required: true },
   stock: { type: Number, required: true },
   imageUrl: { type: String },
+  reviews: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Review' }],
+  averageRating: { type: Number, default: 0 },
+  totalReviews: { type: Number, default: 0 },
 });
 
 module.exports = mongoose.model('Product', productSchema);
