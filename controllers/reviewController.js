248979bdc607d351ad3d12d7959830d39ce2d429
ltxstudio <ltxstudio.info@gmail.com>
2024-11-14 18:33:39 +0600

    Update reviewController.js

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,29 +1,37 @@
 const Review = require('../models/Review');
 const Product = require('../models/Product');
 
-exports.addReview = async (req, res) => {
+exports.addReview = async (req, res, next) => {
   try {
     const { productId, userId, rating, comment } = req.body;
-    const review = new Review({ productId, userId, rating, comment });
-    await review.save();
 
-    // Update product rating
+    const newReview = new Review({ productId, userId, rating, comment });
+    await newReview.save();
+
     const product = await Product.findById(productId);
-    product.reviews.push(review._id);
+    product.reviews.push(newReview._id);
+
+    const totalReviews = product.reviews.length;
+    const averageRating =
+      (product.averageRating * (totalReviews - 1) + rating) / totalReviews;
+
+    product.averageRating = averageRating;
+    product.totalReviews = totalReviews;
+
     await product.save();
 
-    res.json(review);
+    res.json({ success: true, review: newReview });
   } catch (err) {
-    res.status(500).json({ error: err.message });
+    next(err);
   }
 };
 
-exports.getReviews = async (req, res) => {
+exports.getReviews = async (req, res, next) => {
   try {
     const { productId } = req.params;
     const reviews = await Review.find({ productId }).populate('userId', 'username');
     res.json(reviews);
   } catch (err) {
-    res.status(500).json({ error: err.message });
+    next(err);
   }
 };
