187770d6a41647f1b77dba8c9c4538cb06e34933
ltxstudio <ltxstudio.info@gmail.com>
2024-11-14 18:19:22 +0600

    Create cartController.js

---
+++ controllers/cartController.js
@@ -0,0 +1,28 @@
+const Cart = require('../models/Cart');
+const Product = require('../models/Product');
+
+exports.addToCart = async (req, res) => {
+  try {
+    const { userId, productId, quantity } = req.body;
+
+    let cart = await Cart.findOne({ userId });
+    if (!cart) {
+      cart = new Cart({ userId, items: [] });
+    }
+
+    const itemIndex = cart.items.findIndex(item => item.productId.toString() === productId);
+    if (itemIndex > -1) {
+      cart.items[itemIndex].quantity += quantity;
+    } else {
+      const product = await Product.findById(productId);
+      if (!product) return res.status(404).json({ message: 'Product not found' });
+
+      cart.items.push({ productId, quantity });
+    }
+
+    await cart.save();
+    res.json(cart);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
