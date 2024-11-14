cb6aaa820b0489286245c87845ffa26f963de909
ltxstudio <ltxstudio.info@gmail.com>
2024-11-14 18:31:47 +0600

    Update productController.js

--- controllers/productController.js
+++ controllers/productController.js
@@ -18,3 +18,21 @@ exports.createProduct = async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 };
+
+exports.getProducts = async (req, res, next) => {
+  try {
+    const { keyword, minPrice, maxPrice, sort } = req.query;
+
+    const query = {};
+    if (keyword) query.name = { $regex: keyword, $options: 'i' };
+    if (minPrice) query.price = { $gte: parseFloat(minPrice) };
+    if (maxPrice) query.price = { $lte: parseFloat(maxPrice) };
+
+    const sortOrder = sort === 'price_asc' ? { price: 1 } : { price: -1 };
+
+    const products = await Product.find(query).sort(sortOrder);
+    res.json(products);
+  } catch (err) {
+    next(err);
+  }
+};
