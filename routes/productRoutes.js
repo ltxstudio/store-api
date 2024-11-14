e77a680108884b9b21c6bc95ba3a755b2bfc5064
ltxstudio <ltxstudio.info@gmail.com>
2024-11-14 18:26:20 +0600

    Create productRoutes.js

---
+++ routes/productRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const { getProducts, createProduct } = require('../controllers/productController');
+const { authenticate } = require('../middlewares/authMiddleware');
+const router = express.Router();
+
+router.get('/', getProducts);
+router.post('/', authenticate, createProduct);
+
+module.exports = router;
