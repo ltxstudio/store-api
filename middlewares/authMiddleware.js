7d3e74ab1d1481487315baaabbb37d2b1ae7eb3f
ltxstudio <ltxstudio.info@gmail.com>
2024-11-14 18:35:45 +0600

    Update authMiddleware.js

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -2,13 +2,15 @@ const jwt = require('jsonwebtoken');
 
 exports.authenticate = (req, res, next) => {
   const token = req.header('Authorization');
-  if (!token) return res.status(401).json({ message: 'No token, authorization denied' });
+  if (!token) {
+    return res.status(401).json({ message: 'Authentication required' });
+  }
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
     next();
   } catch (err) {
-    res.status(401).json({ message: 'Token is not valid' });
+    return res.status(401).json({ message: 'Invalid token' });
   }
 };
