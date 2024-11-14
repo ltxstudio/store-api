85217ed036673510a0dcb75c5515c0b02df133ee
ltxstudio <ltxstudio.info@gmail.com>
2024-11-14 18:22:29 +0600

    Create productController.js

---
+++ controllers/productController.js
@@ -0,0 +1,20 @@
+const Product = require('../models/Product');
+
+exports.getProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+exports.createProduct = async (req, res) => {
+  try {
+    const newProduct = new Product(req.body);
+    await newProduct.save();
+    res.status(201).json(newProduct);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
